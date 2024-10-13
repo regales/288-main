e3e88fd8abc3b2554f763e46e65630f72f7fc453
Ryan <66663195+regales@users.noreply.github.com>
2024-10-13 15:54:46 -0500

    cleanup-lab5

---
+++ 288-workspace/lab_5/Debug/main.d
@@ -0,0 +1,96 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+main.obj: ../button.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h
+main.obj: ../timer.h
+main.obj: ../lcd.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+main.obj: ../cyBot_uart.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+main.obj: ../cyBot_Scan.h
+main.obj: ../open_interface.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/math.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_defs.h
+main.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_limits.h
+main.obj: ../movement.h
+
+../main.c:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+../button.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h:
+
+../timer.h:
+
+../lcd.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+../cyBot_uart.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+../cyBot_Scan.h:
+
+../open_interface.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/math.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_defs.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_limits.h:
+
+../movement.h:
+
---
+++ 288-workspace/lab_5/button.c
@@ -0,0 +1,167 @@
+/*
+ * button.c
+ *
+ *  Created on: Jul 18, 2016
+ *      Author: Eric Middleton, Zhao Zhang, Chad Nelson, & Zachary Glanz.
+ *
+ *  @edit: Lindsey Sleeth and Sam Stifter on 02/04/2019
+ *  @edit: Phillip Jone 05/30/2019: Mearged Spring 2019 version with Fall 2018
+ */
+
+
+
+//The buttons are on PORTE 3:0
+// GPIO_PORTE_DATA_R -- Name of the memory mapped register for GPIO Port E,
+// which is connected to the push buttons
+#include "button.h"
+
+// Global varibles
+volatile int button_event;
+volatile int button_num;
+
+/**
+ * Initialize PORTE and configure bits 0-3 to be used as inputs for the buttons.
+ */
+void button_init() {
+    static uint8_t initialized = 0;
+
+    //Check if already initialized
+    if(initialized){
+        return;
+    }
+
+    // delete warning after implementing
+    #warning "Unimplemented function: void button_init()"
+
+    // Reading: To initialize and configure GPIO PORTE, visit pg. 656 in the
+    // Tiva datasheet.
+
+    // Follow steps in 10.3 for initialization and configuration. Some steps
+    // have been outlined below.
+
+    // Ignore all other steps in initialization and configuration that are not
+    // listed below. You will learn more about additional steps in a later lab.
+
+    SYSCTL_RCGCGPIO_R |= 0x10;  // Enable Port E clock (bit 4)
+    timer_waitMillis(1);        // Small delay to allow clock to stabilize
+
+     // 2) Set the buttons (PE0-PE3) as inputs
+     GPIO_PORTE_DIR_R &= ~0x0F;  // Set PE0-PE3 as inputs
+
+     // 3) Enable digital functionality for PE0-PE3
+     GPIO_PORTE_DEN_R |= 0x0F;   // Enable digital functionality for PE0-PE3
+
+     initialized = 1;
+}
+
+
+
+/**
+ * Initialize and configure PORTE interupts
+ */
+void init_button_interrupts() {
+
+    #warning: "Unimplemented function: void init_button_interrupts() -- You must configure GPIO to detect interrupts" // delete warning after implementing
+    // In order to configure GPIO ports to detect interrupts, you will need to visit pg. 656 in the Tiva datasheet.
+    // Notice that you already followed some steps in 10.3 for initialization and configuration of the GPIO ports in the function button_init().
+    // Additional steps for setting up the GPIO port to detect interrupts have been outlined below.
+    // TODO: Complete code below
+
+    // 1) Mask the bits for pins 0-3
+    GPIO_PORTE_IM_R &= ~0x0F;
+
+    // 2) Set pins 0-3 to use edge sensing
+    GPIO_PORTE_IS_R &= ~0x0F;
+
+    // 3) Set pins 0-3 to use both edges. We want to update the LCD
+    //    when a button is pressed, and when the button is released.
+    GPIO_PORTE_IBE_R |= 0x0F;
+
+    // 4) Clear the interrupts
+    GPIO_PORTE_ICR_R = 0x0F;
+
+    // 5) Unmask the bits for pins 0-3
+    GPIO_PORTE_IM_R |= 0x0F;
+
+    #warning: "Unimplemented function: void init_button_interrupts() -- You must configure interrupts" // delete warning after implementing
+    // TODO: Complete code below
+    // 6) Enable GPIO port E interrupt
+    NVIC_EN0_R |= (1 << 4);
+
+    // Bind the interrupt to the handler.
+    IntRegister(INT_GPIOE, gpioe_handler);
+}
+
+
+/**
+ * Interrupt handler -- executes when a GPIO PortE hardware event occurs (i.e., for this lab a button is pressed)
+ */
+void gpioe_handler() {
+
+#warning: "Unimplemented function: void gpioe_handler() -- You must configure interrupts" // delete warning after implementing
+    // Clear interrupt status register
+    GPIO_PORTE_ICR_R = 0x0F;
+    button_event = 1;
+    button_num = button_getButton();
+}
+
+
+
+
+/**
+ * Returns the position of the rightmost button being pushed.
+ * @return the position of the rightmost button being pushed. 4 is the rightmost button, 1 is the leftmost button.  0 indicates no button being pressed
+ */
+uint8_t button_getButton() {
+
+    #warning "Unimplemented function: uint8_t button_getButton(void)"   // delete warning after implementing
+
+    //
+    // DELETE ME - How bitmasking works
+    // ----------------------------------------
+    // In embedded programming, often we only care about one or a few bits in a piece of
+    // data.  There are several bitwise operators that we can apply to data in order
+    // to "mask" the bits that we don't care about.
+    //
+    //  | = bitwise OR      & = bitwise AND     ^ = bitwise XOR     ~ = bitwise NOT
+    //        << x = shift left by x bits        >> x = shift right by x bits
+    //
+    // Let's say we want to know if push button 3 (S3) of GPIO_PORTE_DATA_R is
+    // pushed.  Since push buttons are high (1) initially, and low (0) if pushed, PORTE should
+    // look like:
+    // GPIO_PORTE_DATA_R => 0b???? ?0?? if S3 is pushed
+    // GPIO_PORTE_DATA_R => 0b???? ?1?? if S3 is not pushed
+    //
+    // This is not useful: There are 128 different 8 bit numbers that have the 3rd bit high or low.
+    // We can make it more clear if we mask the other 7 bits:
+    //
+    // Bitwise AND:
+    // (GPIO_PORTE_DATA_R & 0b0000 0100) => 0b0000 0000 if S3 is pushed
+    // (GPIO_PORTE_DATA_R & 0b0000 0100) => 0b0000 0100 if S3 is not pushed
+    //
+    // Bitwise OR:
+    // (GPIO_PORTE_DATA_R | 0b1111 1011) => 0b1111 1011 if S3 is pushed
+    // (GPIO_PORTE_DATA_R | 0b1111 1011) => 0b1111 1111 if S3 is not pushed
+    //
+    // Other techniques (Shifting and bitwise AND)
+    // ((GPIO_PORTE_DATA_R >> 2) & 1) => 0 if S3 is pushed
+    // ((GPIO_PORTE_DATA_R >> 2) & 1) => 1 if S3 is not pushed
+
+    // TODO: Write code below -- Return the left must button position pressed
+
+    if ((GPIO_PORTE_DATA_R & 0x08) == 0) {
+        return 4;   // Button 4 (PE3) pressed
+    } else if ((GPIO_PORTE_DATA_R & 0x04) == 0) {
+        return 3;   // Button 3 (PE2) pressed
+    } else if ((GPIO_PORTE_DATA_R & 0x02) == 0) {
+        return 2;   // Button 2 (PE1) pressed
+    } else if ((GPIO_PORTE_DATA_R & 0x01) == 0) {
+        return 1;   // Button 1 (PE0) pressed
+    } else {
+        return 0;   // No button pressed
+    }
+}
+
+
+
+
